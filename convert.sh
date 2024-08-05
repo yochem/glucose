1c67422695f749038f5194e398233f1e932c5bc1
Yochem van Rosmalen <git@yochem.nl>
2024-08-05 13:14:48 +0200

    Remove leading zero from day number

--- convert.sh
+++ convert.sh
@@ -6,7 +6,7 @@ set -u
 command -v gdate >/dev/null && datecmd=gdate || datecmd=date
 
 # use dutch month name
-export date=$(LC_ALL=nl_NL $datecmd -d $input_datetime "+%d %B %Y")
+export date=$(LC_ALL=nl_NL $datecmd -d $input_datetime "+%-d %B %Y")
 export time=$($datecmd -d $input_datetime "+%H:%M")
 export value=$(printf "%.1f" $input_value)
 export workflow_url="https://github.com/yochem/glucose/actions/runs/$input_action_id"
